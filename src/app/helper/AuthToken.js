c67a097850f9887b9984d0e9a82b6fdf27ff5e55
ItaloDavidb <italodavid2@hotmail.com>
2022-02-14 20:43:45 -0300

    ref: code refactor

--- src/app/helper/AuthToken.js
+++ src/app/helper/AuthToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 function TokenAuth(req,res,next){
   const auth = req.headers['authorization'];
   const token = auth && auth.split(' ')[1];
-  if(token === null) return res.status(401).send('Unauthorized Access');
+  if(token === null) return res.status(401).send('Invalid Token, Unauthorized Acess');
 
   try {
     jwt.verify(token, tok.secret);
@@ -13,13 +13,3 @@ function TokenAuth(req,res,next){
   }
 }
 module.exports = TokenAuth;
-// (data) =>{
-//     if(data) return res.status(403);
-//     console.log(data);
-//     // if(!data) return res.status(401).send('Unauthorized Access');
-//     next();
-//   } );
-// jwt.verify(token, 'wrong-secret', function(err, decoded) {
-//     // err
-//     // decoded undefined
-//   });
