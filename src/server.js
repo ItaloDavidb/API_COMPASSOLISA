c1af2d1806f8e258b8ef3cde172c7b8ad47ff7a2
ItaloDavidb <italodavid2@hotmail.com>
2022-02-19 18:46:33 -0300

    ref: code refactor

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const App = require('./app');
 require('dotenv').config();
 
-const port = process.env.DB_PORT;
+const port = process.env.PORT;
 
 App.listen(port, () => console.log(`Server is running on port ${port}`));
